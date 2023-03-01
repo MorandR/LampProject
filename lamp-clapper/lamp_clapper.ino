d8141eb8c9775651daec7859adf9ca4cc187798f
Richard M <70453767+MorandR@users.noreply.github.com>
2023-03-01 12:26:22 -0500

    Update lamp_clapper.ino
    Altered with debugging

--- lamp-clapper/lamp_clapper.ino
+++ lamp-clapper/lamp_clapper.ino
@@ -1,47 +1,54 @@
 
-unsigned long sound = A0; // Sound coming from sensor
-int relay = 4;
-boolean light_on = false;
+unsigned long sound = A0;
+int relay = 2;
 
-int counter = 0;
-int threshold = 500; // Sound threshold
+
+unsigned long clap1 = 0;
+unsigned long clap2 = 0;
+boolean light_on = false;
+unsigned long heard = 0;
 int status = HIGH;
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(sound, INPUT);
   pinMode(relay, OUTPUT);
-  Serial.println("Hello World");
+  Serial.println("Hello Richard");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  heard = analogRead(sound);
 
-  int sound_signal = analogRead(sound);
-  Serial.print(sound_signal);
+  Serial.print("dB level: ");
+  Serial.println(heard);
 
-//  Serial.print(counter + " ");
+  if(heard > 500){
+    clap1 = millis();
+    delay(200);
+    for(int i= 1000; i > 0; i--){
+      if(analogRead(sound) > 500){
+        clap2 = millis();
 
+        Serial.print("Clap 1: ");
+        Serial.println(clap1);
+        Serial.print("Clap 2: ");
+        Serial.println(clap2);
 
-  // Alternate Light
-  if(sound_signal >= threshold) {
-//    Serial.print(sound_signal);
-    light_on = !light_on;
-    status = !status;
-    digitalWrite(relay, status);
-    delay(400);
+        if(clap2-clap1 > 240 && clap2-clap1 < 310){
+          Serial.println("Lights Switched");
+          light_on = !light_on;
+          status = !status;
+          digitalWrite(relay, status);
+          delay(3000);
+        }
+      break;
+      }
+    } 
   }
-
-
-  if(light_on == false) {
-    Serial.println(" Light ON");
-  }
-  else if( light_on == true) { 
-    Serial.println(" Light OFF");
-  }
-
-
-  counter++;
-
 }
