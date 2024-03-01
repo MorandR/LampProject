380bdc6274740dd7b2a823d3508f199c03105a9f
Richard M <70453767+MorandR@users.noreply.github.com>
2024-02-29 21:23:21 -0500

    Update lamp_clapper.ino
    Implemented a second clap to activate.

--- lamp-clapper/lamp_clapper.ino
+++ lamp-clapper/lamp_clapper.ino
@@ -5,6 +5,8 @@ int relay = 2;
 
 unsigned long clap1 = 0;
 unsigned long clap2 = 0;
+unsigned long db_diff = 0;
+int db_range[2] = {300, 500};
 boolean light_on = false;
 unsigned long heard = 0;
 int status = HIGH;
@@ -23,32 +25,44 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  heard = analogRead(sound);
+heard = analogRead(sound);
 
-  Serial.print("dB level: ");
-  Serial.println(heard);
+Serial.print("dB level: ");
+Serial.println(heard);
 
-  if(heard > 500){
-    clap1 = millis();
-    delay(200);
-    for(int i= 1000; i > 0; i--){
-      if(analogRead(sound) > 500){
-        clap2 = millis();
+if(heard > 500){
+  clap1 = millis();
+  delay(200);
+  for(int i= 3000; i > 0; i--){
+    if(analogRead(sound) > 500){
+      clap2 = millis();
 
-        Serial.print("Clap 1: ");
-        Serial.println(clap1);
-        Serial.print("Clap 2: ");
-        Serial.println(clap2);
+      db_diff = clap2 - clap1;
+      
+      Serial.print("Clap 1: ");
+      Serial.println(clap1);
+      Serial.print("Clap 2: ");
+      Serial.println(clap2);
+      Serial.print("Time Diff:");
+      Serial.println(db_diff);
+      
+      if(db_diff > db_range[0] && db_diff < db_range[1]){
+        Serial.println("Lights Switched");
+        light_on = !light_on;
+        status = !status;
+        digitalWrite(relay, status);
+         delay(2800);
 
-        if(clap2-clap1 > 240 && clap2-clap1 < 310){
-          Serial.println("Lights Switched");
-          light_on = !light_on;
-          status = !status;
-          digitalWrite(relay, status);
-          delay(3000);
-        }
-      break;
       }
-    } 
+    break;
+    }
   }
+  
+}
+
+
+
+
+
+
 }
